f62f5dfed9a4d72c56a463321ff634c4e3db2ea0
felipesantosds <felipedssantos.dev@gmail.com>
2021-10-19 12:00:49 -0300

    Add header component

---
+++ src/app/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import HomePage from '../ui/pages/HomePage';
+import './styles.css';
+
+function App() {
+  return (
+    <div className="app">
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/ui/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import Header from '../components/Header';
+
+import '../styles/pages/HomePage.css';
+
+function HomePage() {
+    return (
+        <div id="home-page">
+            <Header />
+        </div>
+    );
+}
+
+export default HomePage;
